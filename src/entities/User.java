56d711d5b36bc399abe5d679e0fbd954bb9d9b24
Jefferson <boarhat2018@gmail.com>
2025-06-26 21:55:52 -0300

    Criação das classes DataBaseRepository e User

---
+++ src/entities/User.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class User {
+    private String name;
+    private String email;
+    private String senha;
+    private String codigoValidacao;
+    
+    public User (String name, String senha){
+        this.name=name;
+        this.senha=senha;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCodigoValidacao() {
+        return codigoValidacao;
+    }
+
+    public void setCodigoValidacao(String codigoValidacao) {
+        this.codigoValidacao = codigoValidacao;
+    }
+    
+    
+}
